0ef71682eb22c3273b1881cf86e8c8854fcb8857
bodarajeshkumar <82795269+bodarajeshkumar@users.noreply.github.com>
2021-10-04 10:49:37 +0530

    Update defaault-adapter.js

--- defaault-adapter.js
+++ defaault-adapter.js
@@ -1,20 +1,24 @@
-Object.keys(vpcJson.paths).forEach(function(path) {
-	Object.keys(vpcJson.paths[path]).forEach(function(operation){
-		if(vpcJson.paths[path][operation].tags && vpcJson.paths[path][operation].tags.includes("Instance groups")>0)
-		{
-			console.log(vpcJson.paths[path][operation].operationId);
-			console.log(vpcJson.paths[path][operation].summary);
-	let apiDetails = vpcJson.paths[path][operation];
-	let output = {};
-	let codeSnippet = {};
-	output.apiName = apiDetails.operationId;
-	output.description = apiDetails.description;
-	output.title = apiDetails.title;
-	output.codeSnippets = [];
-	codeSnippet.language = "python";
-	codeSnippet.codeSnippet = apiDetails["x-sdk-operations"]["request-examples"]["python"][0]["example"][0].source[0];
-	output.codeSnippets.push(codeSnippet);
-	allCodeSnippets.push(output);
-		}
-	});
-});
+var allCodeSnippets = [];
+  Object.keys(vpcJson.paths).forEach(function(path) {
+    Object.keys(vpcJson.paths[path]).forEach(function(operation){
+      if(vpcJson.paths[path][operation].tags && vpcJson.paths[path][operation].tags.includes(tag)>0)
+      {
+        console.log(vpcJson.paths[path][operation].operationId);
+        console.log(vpcJson.paths[path][operation].summary);
+        let apiDetails = vpcJson.paths[path][operation];
+        let output = {};
+        let codeSnippet = {};
+        output.apiName = apiDetails.operationId;
+        output.description = apiDetails.description;
+        output.title = apiDetails.title;
+        output.codeSnippets = [];
+        codeSnippet.language = "python";
+        codeSnippet.codeSnippet = apiDetails["x-sdk-operations"]["request-examples"]["python"][0]["example"][0].source[0];
+        output.codeSnippets.push(codeSnippet);
+        allCodeSnippets.push(output);
+        finalJson.allApis = allCodeSnippets;
+        finalJson.requiredSDKs = [];
+        finalJson.requiredSDKs.push("ibm-vpc>=0.7.0");
+      }
+    });
+  });
